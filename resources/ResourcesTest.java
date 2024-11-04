2b6cc9d361e34ea01ed31f554a7d40935e51a762
Rhenjiromg <167743103+Rhenjiromg@users.noreply.github.com>
2024-11-03 18:17:18 -0800

    feat: Added file systems and resources-client-server layout (#56)

---
+++ resources/ResourcesTest.java
@@ -0,0 +1,8 @@
+//always include this to be able to use it. 
+package resources;
+
+public class ResourcesTest {
+    public static void testGetResources(){
+        System.out.println("Hello from resources");
+    }
+}
