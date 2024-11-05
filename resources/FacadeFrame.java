f9266d21318d77c099fceb3e92cbe4937a8f9836
Alex <ajrojas304@gmail.com>
2024-11-04 17:23:58 -0800

    Added FacadeFrame files to "resources"

---
+++ resources/FacadeFrame.java
@@ -0,0 +1,23 @@
+public class FacadeFrame {
+Message_Facade msg; 
+Client clt;
+
+public FacadeFrame (Message_Facade msg, Client clt) {
+	this.msg = msg;
+	this.clt = clt;
+	
+}
+public void sendMessage(String message) {
+	System.out.println("Preparing to send a message...." + "\n");
+	clt.on();
+	msg.sending();
+}
+
+public void sentMessage() {
+	System.out.println();
+	System.out.println("Message has been sent...." + "\n");
+	clt.off();
+	msg.sent();
+}
+	
+}
