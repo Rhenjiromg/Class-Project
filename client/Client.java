d979173d1a5a0d9d00b25149f1d974c335439e9d
Phuong Nguyen <pastoryuos@outlook.com>
2024-11-08 10:06:08 -0800

    Update Client.java
    + add proper socket handling
    + add message, socket hashmap
    + less pseudo codes, send/listen should work now

--- client/Client.java
+++ client/Client.java
@@ -3,6 +3,7 @@ package client;
 import resources.*;
 import java.io.*;
 import java.net.*;
+import java.util.HashMap;
 
 import java.util.ArrayDeque;
 import java.util.Queue;
@@ -16,6 +17,8 @@ public class Client {
     private int port = 31415;
 	private Queue<Message> outbound = new ArrayDeque<>();
 	private Queue<Message> inbound = new ArrayDeque<>();
+	private HashMap<Message, Socket> receipt = new HashMap<>();
+	
 
     public Client() {
 		this.IP = InetAddress.getInetAddress().getHostAddress();
@@ -48,14 +51,22 @@ public class Client {
         return queue.poll();
     }
 
+	private synchronized void addPair(Message message, Socket socket) { 
+		messageSocketMap.put(message, socket);
+	}
+
+	//this is essentially a pop: get socket from key message then remove it from map
+	private synchronized Socket removePair(Message message) { 
+		return messageSocketMap.remove(message);
+	}
+
 	//send monitor outbound queue and fork a thread to handle each item in queue until it is empty
     private class SendThread implements Runnable {
 		@Override
         public void run() {
 			while(true){ //TODO: update this with better condition instead of inti loop + close stream when it is no longer infi loop
-				ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream()); //object out sender
 				while(!outbound.isEmpty()){
-					new Thread(new Sender(Message)).start();
+					new Thread(new Sender(popQueue(outbound))).start();
 				}
            	} 
 		}
@@ -63,9 +74,21 @@ public class Client {
 
 	//sub thread sen
 	private class Sender implements Runnable {
+		private Message m;
+		private Socket s;
+
+		public Sender(Message msg){
+			this.m = msg;
+			this.s = removePair(msg);
+		}
+
 		@Override
         public void run() {
-			objectOutputStream.writeObject(popQueue());
+			ObjectOutputStream objectOutputStream = new ObjectOutputStream(this.s.getOutputStream()); //object out sender
+			objectOutputStream.writeObject(this.m);
+			objectOutputStream.flush();
+			objectOutputStream.close();
+			this.s.close();
 		}
     }
     
@@ -86,8 +109,8 @@ public class Client {
 	private class Process implements Runnable {
 		@Override
         public void run() {
-			ClientFacade processer = new ClientFacade(popQueue(outbound)); //NOTE: placeholder, ideally we create a facade here in this thread with the Message passed in as args
-			Message result = processer.process(); //placeholder 
+			ClientFacade processer = new ClientFacade(popQueue(inbound)); //NOTE: placeholder, ideally we create a facade here in this thread with the Message passed in as args
+			Message result = processer.process(); //NOTE: placeholder 
 			addQueue(outbound, result);
 		}
     }
@@ -97,21 +120,31 @@ public class Client {
 		@Override
         public void run() {
 
-			ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());
+			ServerSocket serverSocket = new ServerSocket(port);
 			//NOTE: infi loop to listen for object coming in (message), mod later id needed
             while(true){
-				Socket client = server.accept();
-				new Thread(new Listen()).start();
+				Socket client = serverSocket.accept();
+				new Thread(new Listen(client)).start();
 			}
+			serverSocket.close(); //TODO: once loop condition is updated, this will clean up serverSocket
 		}
     }
 
 	//sub thread process
 	private class Listen implements Runnable {
+		private Socket s;
+
+		public Sender(Socket soc){
+			this.s = soc;
+		}
+
 		@Override
 		public void run() {
+			ObjectInputStream objectInputStream = new ObjectInputStream(this.s.getInputStream());
 			Message msg = (Message) objectInputStream.readObject();
 			addQueue(inbound, msg);
+			addPair(msg, s);
+			objectInputStream.close();
 		}
 	}
 }
