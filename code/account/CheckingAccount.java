86285b4acdc51cc259c4b6f8fc7f5d4121ce4e85
Rohan-Kumar09 <143048397+Rohan-Kumar09@users.noreply.github.com>
2024-11-15 14:16:47 -0800

    Fixed to the Account package (#69)

--- code/account/CheckingAccount.java
+++ code/account/CheckingAccount.java
@@ -1,20 +1,15 @@
 package account;
 
 public class CheckingAccount extends Account {
-	private double balance;
 	private final double maintenanceFee = 5.0;
 	
 	
 	public CheckingAccount() {
-		balance = 0;
+		setBalance(0);
 	}
 	
 	public CheckingAccount(double balance) {
-		this.balance = balance;
-	}
-	
-	public double getBalance() {
-		return balance;
+		this.setBalance(balance);
 	}
 	
 	public void deposit(double amount) {
@@ -23,10 +18,6 @@ public class CheckingAccount extends Account {
 	
 	public void withdraw(double amount) {
 		super.withdraw(amount);
-	}
-	
-	public void closeAccount() {
-		super.closeAccount();
 	}
 	
 	public void transferFunds(Account targetAccount) {
@@ -40,7 +31,7 @@ public class CheckingAccount extends Account {
 		// if the same date hits Ex: Nov 14, Dec 14
 		// deduct the maintenanceFee of $5
 		if (creationDate[1].equals(currentDate[1])) {
-			balance -= maintenanceFee;
+			setBalance(getBalance() - maintenanceFee);
 		}
 	}
 }
