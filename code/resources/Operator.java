441e571337ab5e318314b7f291e9c56b2cc093c1
Rhenjiromg <167743103+Rhenjiromg@users.noreply.github.com>
2024-11-23 09:59:15 -0800

    Operator Overhaul (#73)

---
+++ code/resources/Operator.java
@@ -0,0 +1,49 @@
+package resources;
+
+public class Operator {
+    private static int counter = 0;
+
+    private String Name;
+    private String ID;
+    private String Password;
+    private UserState State;
+
+    public Operator(String name, String password) {
+        this.Password = password;
+        this.Name = name;
+        this.State = UserState.NEW;
+        this.ID = Integer.toString(++counter);
+    }
+
+    public Operator(String name, String password, String ID) {
+        this.Password = password;
+        this.Name = name;
+        this.State = UserState.OPEN;
+        this.ID = ID;
+    }
+
+    public boolean Authenticate(String Password) {
+        return this.Password.equals(Password);
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public String getId() {
+        return ID;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public UserState getState() {
+        return State;
+    }
+
+    public void setUserState(UserState state) {
+        this.State = state;
+    }
+
+}
---
+++ code/resources/UserState.java
@@ -0,0 +1,5 @@
+package resources;
+
+enum UserState {
+    NEW, OPEN, CLOSED, ACTIVE, BANNED
+}
